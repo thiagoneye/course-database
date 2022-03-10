8058ff6d2af8322750277530562deeda30b6ac50
Thiago Rodrigues <57311069+thiagoneye@users.noreply.github.com>
2022-03-09 22:51:16 -0300

    Update files (folder 03-sqlite)

--- cap18/03-sqlite/utils.py
+++ cap18/03-sqlite/utils.py
@@ -8,72 +8,114 @@ Aplicação CRUD com Python e SQLite
 
 # Imports
 
+import sqlite3
 
 # Functions
 
 def conectar():
     """
-    Função para conectar ao servidor
+    Função para conectar ao servidor.
     """
-    print('Conectando ao servidor...')
+    conn = sqlite3.connect('psqlite3.db')
 
+    conn.execute("""CREATE TABLE IF NOT EXISTS produtos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        preco REAL NOT NULL,
+        estoque INTEGER NOT NULL
+    );""")
 
-def desconectar():
+    return conn
+
+def desconectar(conn):
     """ 
     Função para desconectar do servidor.
     """
-    print('Desconectando do servidor...')
+    conn.close()
 
-
-def listar():
+def listar(conn):
     """
-    Função para listar os produtos
+    Função para listar os produtos.
     """
-    print('Listando produtos...')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM produtos')
+    produtos = cur.fetchall()
 
+    if len(produtos) > 0:
+        print('Listando produtos ...')
+        for produto in produtos:
+            print('---------------------')
+            print(f'ID: {produto[0]}')
+            print(f'Nome: {produto[1]}')
+            print(f'Preço {produto[2]}')
+            print(f'Quantidade: {produto[3]}')
+    else:
+        print('Não existem produtos cadastrados.')
 
-def inserir():
+def inserir(conn):
     """
-    Função para inserir um produto
-    """  
-    print('Inserindo produto...')
-
-
-def atualizar():
+    Função para inserir um produto.
     """
-    Função para atualizar um produto
+    nome = input('Informe o nome do produto: ')
+    preco = float(input('Informe o preço do produto: '))
+    estoque = int(input('Informe a quantidade do produto: '))
+
+    cur = conn.cursor()
+    cur.execute(f"INSERT INTO produtos (nome, preco, estoque) VALUES ('{nome}', {preco}, {estoque})")
+
+def submeter(conn):
     """
-    print('Atualizando produto...')
-
-
-def deletar():
+    Função para enviar as alterações do banco de dados.
     """
-    Função para deletar um produto
-    """  
-    print('Deletando produto...')
+    conn.commit()
 
+def atualizar(conn):
+    """
+    Função para atualizar um produto.
+    """
+    codigo = int(input('Informe o código do produto a ser atualizado: '))
+    nome = input('Informe o novo nome do produto: ')
+    preco = float(input('Informe o novo preço do produto: '))
+    estoque = int(input('Informe a nova quantidade do produto: '))
+
+    cur = conn.cursor()
+    cur.execute(f"UPDATE produtos SET nome='{nome}', preco ={preco}, estoque={estoque} WHERE id={codigo}")
+
+def deletar(conn):
+    """
+    Função para deletar um produto.
+    """
+    codigo = int(input('Informe o código do produto a ser deletado: '))
+
+    cur = conn.cursor()
+    cur.execute(f"DELETE FROM produtos WHERE id={codigo}")
 
 def menu():
     """
     Função para gerar o menu inicial
     """
+    conn = conectar()
+
     print('=========Gerenciamento de Produtos==============')
     print('Selecione uma opção: ')
     print('1 - Listar produtos.')
     print('2 - Inserir produtos.')
     print('3 - Atualizar produto.')
     print('4 - Deletar produto.')
+
     opcao = int(input())
+
     if opcao in [1, 2, 3, 4]:
         if opcao == 1:
-            listar()
+            listar(conn)
         elif opcao == 2:
-            inserir()
+            inserir(conn)
         elif opcao == 3:
-            atualizar()
-        elif opcao == 4:
-            deletar()
+            atualizar(conn)
         else:
-            print('Opção inválida')
+            deletar(conn)
+
+        submeter(conn)
+        desconectar(conn)
     else:
         print('Opção inválida')
