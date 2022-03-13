6f02c74661d2d608ed9cc71123708a5644b2c81e
Thiago Rodrigues <57311069+thiagoneye@users.noreply.github.com>
2022-03-12 22:38:29 -0300

    Update folder (04-mongodb)

--- cap18/04-mongodb/utils.py
+++ cap18/04-mongodb/utils.py
@@ -8,72 +8,132 @@ Aplicação CRUD com Python e MongoDB
 
 # Imports
 
+import getpass
+from pymongo import MongoClient, errors
+from bson.objectid import ObjectId
 
 # Functions
 
 def conectar():
     """
-    Função para conectar ao servidor
+    Função para conectar ao servidor.
     """
-    print('Conectando ao servidor...')
+    client = MongoClient('localhost', 27017)
 
+    return client
 
-def desconectar():
+def desconectar(client):
     """ 
     Função para desconectar do servidor.
     """
-    print('Desconectando do servidor...')
+    client.close()
 
-
-def listar():
+def listar(client):
     """
     Função para listar os produtos
     """
-    print('Listando produtos...')
+    db = client.pmongo
 
+    try:
+        if db.produtos.count_documents({}) > 0:
+            produtos = db.produtos.find()
+            print('Listando produtos ...')
 
-def inserir():
+            for produto in produtos:
+                print('---------------------')
+                print(f"ID: {produto['_id']}")
+                print(f"Produto: {produto['nome']}")
+                print(f"Preço: {produto['preco']}")
+                print(f"Estoque: {produto['estoque']}")
+        else:
+            print('Não existem produtos cadastrados.')
+    except errors.PyMongoError as err:
+        print(f'Erro ao acessar o banco de dados: {err}')
+
+def inserir(client):
     """
-    Função para inserir um produto
+    Função para inserir um produto.
+    """
+    nome = input('Informe o nome do produto: ')
+    preco = float(input('Informe o preço do produto: '))
+    estoque = int(input('Informe o estoque do produto: '))
+
+    db = client.pmongo
+
+    try:
+        db.produtos.insert_one(
+            {
+                'nome': nome,
+                'preco': preco,
+                'estoque': estoque
+            }
+        )
+        print('O produto foi inserido com sucesso.')
+    except errors.PyMongoError as err:
+        print(f'Não foi possível inserir o produto: {err}')
+
+def atualizar(client):
+    """
+    Função para atualizar um produto.
+    """
+    codigo = input('Informe o ID do produto a ser atualizado: ')
+    nome = input('Informe o novo nome do produto: ')
+    preco = float(input('Informe o novo preço do produto: '))
+    estoque = int(input('Informe o novo estoque do produto: '))
+
+    db = client.pmongo
+
+    try:
+        res = db.produtos.update_one(
+            {'_id': ObjectId(codigo)}, {
+                    '$set': {
+                        'nome': nome,
+                        'preco': preco,
+                        'estoque': estoque
+                    }
+                }
+        )
+        if res.modified_count == 1:
+            print(f'O produto {nome} foi atualizado com sucesso.')
+    except errors.PyMongoError as err:
+        print(f'Não foi possível atualizar o banco de dados: {err}')
+
+def deletar(client):
+    """
+    Função para deletar um produto.
     """  
-    print('Inserindo produto...')
+    codigo = input('Informe o ID do produto a ser deletado: ')
 
+    db = client.pmongo
 
-def atualizar():
-    """
-    Função para atualizar um produto
-    """
-    print('Atualizando produto...')
-
-
-def deletar():
-    """
-    Função para deletar um produto
-    """  
-    print('Deletando produto...')
-
+    try:
+        res = db.produtos.delete_one({'_id': ObjectId(codigo)})
+    except errors.PyMongoError as err:
+        print(f'Não foi possível deletar o produto: {err}')
 
 def menu():
     """
-    Função para gerar o menu inicial
+    Função para gerar o menu inicial.
     """
+    client = conectar()
+
     print('=========Gerenciamento de Produtos==============')
     print('Selecione uma opção: ')
     print('1 - Listar produtos.')
     print('2 - Inserir produtos.')
     print('3 - Atualizar produto.')
     print('4 - Deletar produto.')
+
     opcao = int(input())
+
     if opcao in [1, 2, 3, 4]:
         if opcao == 1:
-            listar()
+            listar(client)
         elif opcao == 2:
-            inserir()
+            inserir(client)
         elif opcao == 3:
-            atualizar()
-        elif opcao == 4:
-            deletar()
+            atualizar(client)
         else:
-            print('Opção inválida')
+            deletar(client)
     else:
         print('Opção inválida')
